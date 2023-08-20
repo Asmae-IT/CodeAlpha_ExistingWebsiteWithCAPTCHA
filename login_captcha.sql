5d64d7079031a29218de7b338de855819911b785
Aissaoui Asmae <61624922+Asmae-IT@users.noreply.github.com>
2023-08-20 11:44:56 +0100

    Update and rename login_admin.sql to login_captcha.sql

--- login_captcha.sql
+++ login_captcha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 18 août 2023 à 00:03
+-- Généré le : dim. 20 août 2023 à 11:39
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
